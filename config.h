5697a314d26f78193199629096fd431a7e749c74
Joshua <joshuamatthewhagen@yahoo.com>
2022-11-03 22:25:28 -0500

    Change topbar padding

--- config.h
+++ config.h
@@ -8,7 +8,7 @@ static const unsigned int gappx	    = 3;
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int topbar_padding	    = 8;        /* default spacing around the bars font */
+static const int topbar_padding	    = 12;        /* default spacing around the bars font */
 static const char *fonts[]          = { "Hack:size=12" };
 static const char dmenufont[]       = "Hack:size=12";
 static const char col_gray1[]       = "#222222";
