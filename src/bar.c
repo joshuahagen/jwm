77b44827850e53aafcfc5bdea71c4c45350fff82
Joshua <joshuamatthewhagen@yahoo.com>
2022-11-29 15:41:56 -0600

    Move tag box to the top right corner of the tag... The tag box is a little square that appears in the tag to let the user know when there is atleast 1 window open under that tag. This is getting moved to the top right corner so it doesn't get cut off on the first tag if the bars corners are rounded.

--- src/bar.c
+++ src/bar.c
@@ -48,9 +48,9 @@ void draw_bar(monitor_t *m)
 		w = TEXTW(tags[i]);
 		drw_setscheme(drw, scheme[m->tagset[m->seltags] & 1 << i ? SchemeSel : SchemeNorm]);
 		drw_text(drw, x, 0, w, bh, lrpad / 2, tags[i], urg & 1 << i);
-		
+
 		if (occ & 1 << i)
-			drw_rect(drw, x + boxs, boxs, boxw, boxw,
+			drw_rect(drw, x + w - boxw, boxs, boxw, boxw,
 				m == selmon && selmon->sel && selmon->sel->tags & 1 << i,
 				urg & 1 << i);
 
@@ -84,7 +84,7 @@ void draw_bar(monitor_t *m)
 								(bh - m->sel->ich) / 2, m->sel->icw, m->sel->ich, m->sel->icon);
 
 				if (m->sel->isfloating)
-					drw_rect(drw, x + boxs, boxs, boxw, boxw, m->sel->isfixed, 0);
+					drw_rect(drw, x + w - boxw, boxs, boxw, boxw, m->sel->isfixed, 0);
 			}
 		} else {
 			drw_setscheme(drw, scheme[SchemeNorm]);
